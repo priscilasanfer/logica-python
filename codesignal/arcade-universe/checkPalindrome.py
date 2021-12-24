ca90d1a8f609507fa975fa88c9cae299bcbbaabd
Priscila Ferreira <priscilasanfer@gmail.com>
2021-12-24 09:00:00 -0300

    Desafio The Journey Begins - 3

---
+++ codesignal/arcade-universe/checkPalindrome.py
@@ -0,0 +1,17 @@
+'''
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+
+'''
+
+
+def solution(inputString):
+    return inputString == inputString[::-1]
